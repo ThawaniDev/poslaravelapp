52dc56851f5409d4d7b87fe9853f22a78daabd74
Abdelsalam Dogorshom <saloom.dogorshom@gmail.com>
2026-04-06 20:59:59 +0300

    pep

--- app/Domain/ContentOnboarding/Enums/ChallengeType.php
+++ app/Domain/ContentOnboarding/Enums/ChallengeType.php
@@ -6,6 +6,7 @@ enum ChallengeType: string
 {
     case PurchaseCount = 'purchase_count';
     case SpendAmount = 'spend_amount';
+    case Spending = 'spending';
     case CategoryExplore = 'category_explore';
     case Streak = 'streak';
 }
